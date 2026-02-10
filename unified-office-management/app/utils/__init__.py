5ab2c0eb277a5600259e4d5f267b9a74495013f7
Tirth Doshi <122155672+DarkSchneider544@users.noreply.github.com>
2026-02-10 15:41:17 +0530

    Delete unified-office-management directory

--- unified-office-management/app/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .response import create_response, create_paginated_response
-from .validators import validate_company_email
-from .helpers import generate_employee_id
-
-__all__ = [
-    "create_response", "create_paginated_response",
-    "validate_company_email", "generate_employee_id"
-]
